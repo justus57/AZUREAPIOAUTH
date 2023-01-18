067fb32d7a36d8c57ea2ee6a7947434a7144f3af
kasyoki.justus <justuskasyoki57@gmail.com>
2023-01-18 10:40:52 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using RestSharp;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetAuthorizationToken();
+            Console.WriteLine(AzureDetails.AccessToken);
+            var client = new RestClient("https://api.businesscentral.dynamics.com/v2.0/4dfedb10-35ca-4e46-9c2a-0fa40d6968c0/Sandbox2/WS/NGO%20Demo/Codeunit/WebPortal");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Authorization", "Bearer "+AzureDetails.AccessToken);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+        }
+
+        public static void GetAuthorizationToken()
+        {
+            ClientCredential cc = new ClientCredential(AzureDetails.ClientID, AzureDetails.ClientSecret);
+           // var context = new AuthenticationContext("https://login.microsoftonline.com/" + AzureDetails.TenantID);
+            var context = new AuthenticationContext("https://login.windows.net/" + AzureDetails.TenantID);
+            var result = context.AcquireTokenAsync("https://api.businesscentral.dynamics.com", cc);
+            if (result == null)
+            {
+                throw new InvalidOperationException("Failed to obtain the Access token");
+            }
+            AzureDetails.AccessToken = result.Result.AccessToken;
+        }
+    }
+}
